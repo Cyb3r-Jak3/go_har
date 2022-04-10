011631e7d75f853b4e8eaba53fad4c242b5201a4
Jacob White <jake@jwhite.network>
2022-04-10 12:25:01 -0400

    Cleanup

--- example/simple/main.go
+++ example/simple/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// Run from project root. To run from folder replace with "../../testdata/Firefox.har"
 	har, err := har.ParseHar("testdata/Firefox.har")
 	if err != nil {
 		log.Fatal(err)
